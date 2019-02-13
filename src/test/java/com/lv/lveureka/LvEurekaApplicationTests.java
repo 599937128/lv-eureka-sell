1adc68ee89264ccb2a0964452a0823628ab8a57e
lvliao <git@code.aliyun.com:te_lv02/test2.git>
2019-02-13 17:51:25 +0800

    创建eureka服务工程,springcloud版本为Finchley.RELEASE

---
+++ src/test/java/com/lv/lveureka/LvEurekaApplicationTests.java
@@ -0,0 +1,17 @@
+package com.lv.lveureka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LvEurekaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
